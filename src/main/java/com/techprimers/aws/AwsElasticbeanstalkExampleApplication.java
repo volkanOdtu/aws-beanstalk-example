49e382e7f6c777d5a4d68d022faffe70b2d54072
AKKA\Volkan.SUYABATMAZ <volkan.suyabatmaz@akka.eu>
2021-04-04 14:36:59 +0300

    0k

---
+++ src/main/java/com/techprimers/aws/AwsElasticbeanstalkExampleApplication.java
@@ -0,0 +1,13 @@
+package com.techprimers.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsElasticbeanstalkExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsElasticbeanstalkExampleApplication.class, args);
+    }
+
+}
